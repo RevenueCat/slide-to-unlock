1ba77e69f0039af73a473f50f97f764753c91e49
skydoves <skydoves@naver.com>
2025-08-22 12:32:19 +0900

    Prepare for release 1.0.0

--- buildSrc/src/main/kotlin/com/revenuecat/slidetounlock/Configuration.kt
+++ buildSrc/src/main/kotlin/com/revenuecat/slidetounlock/Configuration.kt
@@ -4,11 +4,11 @@ object Configuration {
   const val compileSdk = 36
   const val targetSdk = 36
   const val minSdk = 23
-  const val majorVersion = 0
-  const val minorVersion = 1
+  const val majorVersion = 1
+  const val minorVersion = 0
   const val patchVersion = 0
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 1
+  const val versionCode = 2
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.revenuecat.purchases"
 }
